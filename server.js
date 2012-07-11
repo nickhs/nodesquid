ee133ada079d6e7a3993d5e5ca1d46dd6242391b
Nick HS <nickhs@umich.edu>
2012-07-11 09:42:43 -0700

    Kill exiisting afplay instance before starting new one

--- server.js
+++ server.js
@@ -11,91 +11,96 @@ var io = require('socket.io').listen(app);
 var spawn = require('child_process').spawn;
 
 app.get('/', function(req, res) {
-    res.send('Hey there (: ');
+  res.send('Hey there (: ');
 });
 
 app.get('/test/', function(req, res) {
-    var f = fs.readFileSync('client.html');
-    res.writeHead(200, {'Content-Type': 'text/html' });
-    res.end(f);
+  var f = fs.readFileSync('client.html');
+  res.writeHead(200, {'Content-Type': 'text/html' });
+  res.end(f);
 });
 
 app.get('/youtube/*', function(req, res) {
-    var search = req.params[0];
-    res.send('You asked for ' + search);
+  var search = req.params[0];
+  res.send('You asked for ' + search);
 
-    var qoptions = {
-            'q': search,
-            'orderby': 'relevance',
-            'max-results': 1,
-            'key': API_KEY,
-            'alt': 'json'
-        };
+  var qoptions = {
+    'q': search,
+    'orderby': 'relevance',
+    'max-results': 1,
+    'key': API_KEY,
+    'alt': 'json'
+  };
 
-    var query = querystring.stringify(qoptions);
+  var query = querystring.stringify(qoptions);
 
-    var options = {
-        host: "gdata.youtube.com",
-        path: "/feeds/api/videos?" + query
-    }
+  var options = {
+    host: "gdata.youtube.com",
+    path: "/feeds/api/videos?" + query
+  }
 
-    var req = https.get(options, function(res) {
-        var pageData = "";
-        res.on('data', function(chunk) {
-            pageData += chunk;
-        });
-
-        res.on('end', function() {
-            var response = JSON.parse(pageData);
-            var url = response.feed.entry[0].link[0].href;
-            processYoutube(url)
-        });
+  var req = https.get(options, function(res) {
+    var pageData = "";
+    res.on('data', function(chunk) {
+        pageData += chunk;
     });
+
+    res.on('end', function() {
+      var response = JSON.parse(pageData);
+      var url = response.feed.entry[0].link[0].href;
+      processYoutube(url);
+    });
+  });
 });
 
 function processYoutube(url) {
-    var youtube = spawn('youtube-dl', ['--extract-audio', url]);
-    
-    var fileID = "";
+  var youtube = spawn('youtube-dl', ['--extract-audio', url]);
+  
+  var fileID = "";
 
-    youtube.stdout.on('data', function(data) {
-        data = String(data)
-        console.log(data);
-        io.sockets.emit('news', data);
-        if (data.indexOf('ffmpeg') != -1) {
-            data = data.split(':');
-            fileID = data[data.length-1].replace(' ', '');
-        }
-    });
+  youtube.stdout.on('data', function(data) {
+    data = String(data);
+    console.log(data);
+    io.sockets.emit('news', data);
+    if (data.indexOf('ffmpeg') != -1) {
+      data = data.split(':');
+      fileID = data[data.length-1].replace(' ', '');
+    }
+  });
 
-    youtube.on('exit', function(code) {
-        console.log("Finished!");
-        console.log("File ID is: "+fileID);
-        var path = __dirname + "/" + fileID.replace('\n', '');
-        console.log(path);
-        var player = spawn('afplay', [path]);
+  youtube.on('exit', function(code) {
+    console.log("Finished!");
+    console.log("File ID is: "+fileID);
+    var path = __dirname + "/" + fileID.replace('\n', '');
+    console.log(path);
+    playSong(path);
+  });
 
-        player.stdout.on('data', function(data) {
-          console.log('stdout: ' + data);
-        });
+  youtube.stderr.on('data', function(data) {
+    console.log("MAN DOWN!");
+    console.log(data);
+  });
+}
 
-        player.stderr.on('data', function(data) {
-          console.log('stderr: ' + data);
-        });
+function playSong(path) {
+  spawn('killall', ['afplay']);
+  var player = spawn('afplay', [path]);
 
-        player.on('exit', function(code) {
-          console.log('child process existed with code: ' + code);
-        });
-    });
+  player.stdout.on('data', function(data) {
+    console.log('stdout: ' + data);
+  });
 
-    youtube.stderr.on('data', function(data) {
-        console.log("MAN DOWN!");
-        console.log(data);
-    });
+  player.stderr.on('data', function(data) {
+    console.log('stderr: ' + data);
+  });
+
+  player.on('exit', function(code) {
+    console.log('child process existed with code: ' + code);
+  });
 }
 
 io.sockets.on('connection', function(socket) {
-    console.log("Client connected!");
+  console.log("Client connected!");
 });
 
 app.listen(8000);
